2aaa1f6a312d16a406eea9d38b0e21f8d6a628c4
Paul Stack <public@paulstack.co.uk>
2016-02-02 18:44:06 -0500

    sql: Add server, firewall and database commands
    New commands:
        - CreateOrUpdateDatabase
        - CreateOrUpdateFirewallRule
        - CreateOrUpdateServer
        - DeleteDatabase
        - DeleteFirewallRule
        - DeleteServer
        - GetDatabase
        - GetFirewallRule
        - GetServer

---
+++ sql/api.go
@@ -0,0 +1,24 @@
+package sql
+
+import "fmt"
+
+const apiVersion = "2014-04-01-preview"
+const apiProvider = "Microsoft.Sql"
+
+func sqlServerDefaultURLPath(resourceGroupName, serverName string) func() string {
+	return func() string {
+		return fmt.Sprintf("resourceGroups/%s/providers/%s/servers/%s", resourceGroupName, apiProvider, serverName)
+	}
+}
+
+func sqlDatabaseDefaultURLPath(resourceGroupName, serverName, databaseName string) func() string {
+	return func() string {
+		return fmt.Sprintf("resourceGroups/%s/providers/%s/servers/%s/databases/%s", resourceGroupName, apiProvider, serverName, databaseName)
+	}
+}
+
+func sqlServerFirewallDefaultURLPath(resourceGroupName, serverName, firewallRuleName string) func() string {
+	return func() string {
+		return fmt.Sprintf("resourceGroups/%s/providers/%s/servers/%s/firewallRules/%s", resourceGroupName, apiProvider, serverName, firewallRuleName)
+	}
+}
