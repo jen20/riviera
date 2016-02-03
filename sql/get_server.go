28b92807afb07214340922222bc1be303bd9e4f4
James Nugent <james@jen20.com>
2016-02-03 18:03:21 -0500

    sql: Add tags to GetServerResponse
    Note this API is completely undocumented so it's not certain under what
    conditions tags will be returned. There is no harm in adding it though
    - if none are returned, a nil pointer will result in the response
      struct.

--- sql/get_server.go
+++ sql/get_server.go
@@ -3,17 +3,18 @@ package sql
 import "github.com/jen20/riviera/azure"
 
 type GetServerResponse struct {
-	ID                         *string `mapstructure:"id"`
-	Name                       *string `mapstructure:"name"`
-	Location                   *string `mapstructure:"location"`
-	Kind                       *string `mapstructure:"kind"`
-	FullyQualifiedDomainName   *string `mapstructure:"fullyQualifiedDomainName"`
-	AdministratorLogin         *string `mapstructure:"administratorLogin"`
-	AdministratorLoginPassword *string `mapstructure:"administratorLoginPassword"`
-	ExternalAdministratorLogin *string `mapstructure:"externalAdministratorLogin"`
-	ExternalAdministratorSid   *string `mapstructure:"externalAdministratorSid"`
-	Version                    *string `mapstructure:"version"`
-	State                      *string `mapstructure:"state"`
+	ID                         *string             `mapstructure:"id"`
+	Name                       *string             `mapstructure:"name"`
+	Location                   *string             `mapstructure:"location"`
+	Tags                       *map[string]*string `mapstructure:"tags"`
+	Kind                       *string             `mapstructure:"kind"`
+	FullyQualifiedDomainName   *string             `mapstructure:"fullyQualifiedDomainName"`
+	AdministratorLogin         *string             `mapstructure:"administratorLogin"`
+	AdministratorLoginPassword *string             `mapstructure:"administratorLoginPassword"`
+	ExternalAdministratorLogin *string             `mapstructure:"externalAdministratorLogin"`
+	ExternalAdministratorSid   *string             `mapstructure:"externalAdministratorSid"`
+	Version                    *string             `mapstructure:"version"`
+	State                      *string             `mapstructure:"state"`
 }
 
 type GetServer struct {
