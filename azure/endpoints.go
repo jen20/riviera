fc629a0965cc726cd466bbb17caff710d4da01e7
Oliver Arafat <olarafat@microsoft.com>
2016-10-11 22:24:22 +0200

    supporting name and display name format of location

--- azure/endpoints.go
+++ azure/endpoints.go
@@ -1,5 +1,7 @@
 package azure
 
+import "strings"
+
 type Endpoints struct {
 	resourceManagerEndpointUrl string
 	activeDirectoryEndpointUrl string
@@ -8,15 +10,14 @@ type Endpoints struct {
 func GetEndpoints(location string) Endpoints {
 	var e Endpoints
 
+	location = strings.Replace(strings.ToLower(location), " ", "", -1)
+
 	switch location {
-	case GermanyCentral:
-	case GermanyEast:
+	case GermanyCentral, GermanyEast:
 		e = Endpoints{"https://management.microsoftazure.de", "https://login.microsoftonline.de"}
-	case ChinaEast:
-	case ChinaNorth:
+	case ChinaEast, ChinaNorth:
 		e = Endpoints{"https://management.chinacloudapi.cn", "https://login.chinacloudapi.cn"}
-	case USGovIowa:
-	case USGovVirginia:
+	case USGovIowa, USGovVirginia:
 		e = Endpoints{"https://management.usgovcloudapi.net", "https://login.microsoftonline.com"}
 	default:
 		e = Endpoints{"https://management.azure.com", "https://login.microsoftonline.com"}
