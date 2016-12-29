9499031726b15f5c694b451796b879cbf4d8fed6
tombuildsstuff <git@ibuildstuff.co.uk>
2016-12-29 15:07:40 +0000

    Added a means of checking the state for a resource provider

---
+++ azure/get_resource_provider.go
@@ -0,0 +1,29 @@
+package azure
+
+import "fmt"
+
+// SkuName enumerates the values for sku name.
+type RegistrationState string
+
+type GetResourceProviderResponse struct {
+	ID                *string `mapstructure:"id"`
+	Namespace         *string `mapstructure:"namespace"`
+	RegistrationState *string `mapstructure:"registrationState"`
+}
+
+type GetResourceProvider struct {
+	Namespace string `json:"-"`
+}
+
+func (command GetResourceProvider) APIInfo() APIInfo {
+	return APIInfo{
+		APIVersion: resourceGroupAPIVersion,
+		Method:     "GET",
+		URLPathFunc: func() string {
+			return fmt.Sprintf("providers/%s", command.Namespace)
+		},
+		ResponseTypeFunc: func() interface{} {
+			return &GetResourceProviderResponse{}
+		},
+	}
+}
